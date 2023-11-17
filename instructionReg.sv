c0d7dc0e79ad72ccaeb743b47860e670f3fa7368
trungnguyenn43 <trungnguyenn2003@gmail.com>
2023-11-17 17:49:20 -0600

    Added comment and rearrange some lines for visual

--- instructionReg.sv
+++ instructionReg.sv
@@ -1,8 +1,29 @@
+/*
+Module name: instructionReg
+Description: store instruction signal
+By: Leo Ostiga
+Date: 11/3/2023
+
+In: 
+	[N-1:0] D = Data Bus inputs
+	E (Enable) = Enable input
+	CLKb = CLK input
+
+Out:
+	[N-1:0] Q = data bus out signals 
+
+Quick copy:
+instructionReg(.Q() , .D(), .CLKb(), .E());
+*/
+
 module instructionReg#(
 	parameter N = 10 //variable width register
 )
-(input logic [N-1:0] D, E, CLKb,
-output logic [N-1:0] Q);
+(
+	input logic [N-1:0] D, 
+	input logic CLKb, E,
+	output logic [N-1:0] Q
+);
 
 	always_ff@(negedge CLKb)
 	begin 
