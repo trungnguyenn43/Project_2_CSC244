0a68d8febee03b1ac7568814beb0411a73ddf8a3
trungnguyenn43 <trungnguyenn2003@gmail.com>
2023-11-17 18:53:24 -0600

    Added debouncer for CLK and PK

--- Project_2_CSC244.sv
+++ Project_2_CSC244.sv
@@ -28,18 +28,25 @@ module Project_2_CSC244(
 	input logic CLK, CLK50MHz, PKb,
 	
 	output logic [9:0] OUT_DATA_BUS,
-	output logic [6:0] DHEX [2:0], //this is an array to use it, like this DHEX[0][0]
+	output logic [6:0] DHEX [2:0], //this is an array, like this DHEX[0][0]
 	output logic [6:0] THEX,
 	output logic Done
 
 );
+	
+	//Data bus
+	logic [9:0] BUS;
+	
+	/*Debouncer quick copy:
+		debouncer (.A(), .A_noisy(), .CLK50M());
+	*/
+	logic CLKDb; //CLK after debounced
+	debouncer CLKDebouncer(.A(CLKDb), .A_noisy(CLK), .CLK50M(CLK50MHz))
+
+	logic PKdb; //PKdb -> peek
+	debouncer PKDebouncer(.A(PKdb), .A_noisy(PKb), .CLK50M(CLK50MHz));
 
 	
-	logic hello;
-	assign hello = 1;
-
-
-
-
+	
 
 endmodule
