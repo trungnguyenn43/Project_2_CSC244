54ca0194cdeb0a268af4b357fef881ef7afdd5bd
trungnguyenn43 <trungnguyenn2003@gmail.com>
2023-11-26 15:02:06 -0600

    Added op code

--- multiStageALU.sv
+++ multiStageALU.sv
@@ -1,3 +1,24 @@
+/*
+Module name: ALU
+Description: multi stage ALU
+By: Norman Nguyen & Leo Ostiga
+Date: 11/3/2023
+
+In: 
+	[N-1:0] OP = opcode
+	FN = 
+	CLKb = CLK input
+	Gin = 
+	Gout = 
+	Ain = 
+	
+Out:
+	[N-1:0] Q = data bus out signals 
+
+Quick copy:
+instructionReg(.Q() , .D(), .CLKb(), .E());
+*/
+
 module ALU#(
 	parameter N = 10
 )
@@ -5,29 +26,43 @@ module ALU#(
   input logic [N-1:0] OP ,
   input logic [3:0] FN ,
   input logic Ain , Gin , Gout , CLKb ,
-  output logic [N-1:0] Q
+  output logic [N-1:0] RES
 );
 	
 	parameter ADD = 4'b0010;
 	parameter SUB = 4'b0011;
+	parameter INV = 4'b0100;
+	parameter FLP = 4'b0101;
 	parameter AND = 4'b0110;
 	parameter OR = 4'b0111;
+	
 	parameter XOR = 4'b1000;
-  // do the rest of parameters (should be ~12)
+	parameter LSL = 4'b1001;
+	parameter LSR = 4'b1010;
+	parameter ASR = 4'b1011;
+	
   
-  logic [N-1:0] addSubResult;
-	"addsub module goes here" sum(.A(A), .B(B), .C(ALUControl[0]), .S(addSubResult));
+  logic [N-1:0] addSubResult; //temp addOrSub 
+
+  fulladder sum(.A(A), .B(B), .Co(ALUControl[0]), .S(addSubResult));
 	
 	
 	
 	always_comb begin
 		//ALU cases mux
 		case(FN)
-			ADD: Q = addSubResult;
-			SUB: Q = addSubResult;
-			AND: Q = A & B; //andOrResult;
-			OR: Q = A | B;
-      XOR: Q =
+		
+			ADD: RES = addSubResult;
+			SUB: RES = addSubResult;
+			INV: RES = ~B;
+			FLP: RES = ~B;
+			AND: RES = A & B; //andOrResult;
+			OR: RES = A | B;
+			XOR: RES = A ^ B;
+			LSL: 
+			LSR:
+			ASR: 
+			
 		endcase
 	end
 	
