63d1494022155a56c17758d1ad2efd6da9c5d2b5
Leo Ostigaard <105457849+losti3736@users.noreply.github.com>
2023-11-27 22:15:30 -0600

    Update controller.sv

--- controller.sv
+++ controller.sv
@@ -1,160 +1,152 @@
 module controller(
 	input logic [9:0] INSTR,
 	input logic [1:0] T,
-	output logic [5:0] IMM,
+	output logic [9:0] IMM,
 	output logic [1:0] Rin, Rout,
 	output logic ENW, ENR, Ain, Gin, Gout, 
 	output logic [3:0] ALUcont,
 	output logic Ext, IRin, Clr
 );
 
-	/*
-	* create a sequential controller
-	* 	need a counter to keep track of current step (sequential logic!) - always_ff
-	*	need a combinational logic circuit to assign outputs based on current step - always_comb
-	*/
-	
-	
-	logic [1:0] sigBits;
-	assign sigBits= INSTR[9:8];
-	logic [3:0] opINSTR;
-	assign opINSTR= INSTR[3:0];
+	logic sigBit;
+	assign sigBit= INSTR[9];
+	logic [3:0] FN;
+	assign FN= INSTR[3:0];
 	logic [1:0] rx, ry;
 	assign rx = INSTR[7:6];
 	assign ry = INSTR[5:4];
-	logic isLoad, isCopy, isOP1, isOP2, isOP3;
 	
+	parameter
+		LOAD = 4'b0000,
+		COPY = 4'b0001,
+		ADD = 4'b0010,
+		SUB = 4'b0011, 
+		INV = 4'b0100,
+		FLP = 4'b0101, 
+		AND = 4'b0110, 
+		OR = 4'b0111,
+		XOR = 4'b1000,
+		LSL = 4'b1001,
+		LSR = 4'b1010,
+		ASR = 4'b1011;
+		
+	always_comb begin
 	
+	//DEFAULT VALUES FOR CONTROLLER
+	Rin= 2'b00; Rout= 2'b00; ALUcont= 4'b0000; ENW= 1'b0; ENR= 1'b0; IRin= 1'b0; Ext= 1'b0; 
+	Ain= 1'b0; Gin= 1'b0; Gout= 1'b0; Clr= 1'b0; IMM= 10'bz;
 	
-	always_comb
 		case(T)
 		//timesteps 1-4
 			//1ST TS
 			2'b00: begin
-				IRin = 1;
-				Ext = 1;
-				Clr = 0;
-				ENW = 0;
-				ENR = 0;
-				Ain = 0;
-				Gin = 0;
-				Gout = 0;
+				IRin = 1; Ext = 1;
 			end
 			//2ND TS
 			2'b01: begin
-				IRin = 0;
-					
-				case(INSTR)
-					//LOAD
-					10'b00????0000: begin					
-						//Ext= 1;
-						ENW= 1;
-						Clr= 1;
+				
+				if(sigBit) begin
+					ENR= 1;
+					Ain= 1;
+					Rout= rx;
+				end
+				else begin
+				case(FN)
+					LOAD: 
+					begin					
+						Ext= 1; Rin= rx; ENW= 1; Clr= 1;
 					end
-					//COPY
-					10'b00????0001: begin
-						Ext= 0;
-						ENW= 1;
-						ENR= 1;
-						Clr= 1;
+					COPY: 
+					begin
+						Rout= ry; Rin= rx; ENW= 1;	ENR= 1; Clr= 1;
 					end
-					//INV & FLP
-					10'b00????0100, 10'b00????0101: begin
-						Rout= ry;
-						Ext= 0;
-						ENR= 1;
-						Gin= 1;
-						ALUcont = opINSTR; //assign val for FN 
+					INV, FLP: 
+					begin
+						Rout= ry; ENR= 1; Gin= 1;
+						if(INV)
+							ALUcont= INV;
+						else
+							ALUcont= FLP;
 					end
-					//ADD/SUB IMM
-					10'b10????????, 10'b11????????: begin					
-						IMM= INSTR[5:0];
-						Ain= 1;
+					ADD, SUB, AND, OR, XOR, LSL, LSR, ASR: 
+					begin
+						Rout= rx; ENR= 1; Ain= 1; 
 					end
-					default: begin
-						Rout= ry;
-						Ext= 0;
-						ENR= 1;
-						Ain= 1;
-						ALUcont = opINSTR; //assign val for FN
-					end
-					
 				endcase
-					
+				end
 			end
 			//3RD TS
 			2'b10: begin
-				case(INSTR)
-					
-					//LOAD & COPY SHOULD NEVER GET 
-					//TO THE 3RD OR 4TH CLOCK PULSE
-					
-					//INV & FLP
-					10'b00????0100, 10'b00????0101: begin
-						Rin= rx;
-						ENR= 0;
-						Gin= 0;
-						Gout= 1;
-						ENW= 1;
-						Clr= 1;
-						ALUcont = opINSTR; //assign val for FN
+				//LOAD & COPY SHOULD NEVER GET 
+				//TO THE 3RD OR 4TH CLOCK PULSE
+				
+				if(sigBit) begin
+					Gin= 1;
+					IMM= {4'b0, INSTR[5:0]};
+					if(INSTR[8])
+						ALUcont= SUB;
+					else
+						ALUcont= ADD;
+				end
+				case(FN)
+					ADD:
+					begin
+						Rout= ry; ENR= 1; Gin= 1;
+						ALUcont= ADD;
 					end
-					//ADD/SUB IMM
-					10'b10????????, 10'b11????????: begin
-						//IMM= INSTR[5:0];
-						Ain= 0;
-						Gin= 1;
-						ENR= 1;
-						Rout= rx;
-						
+					SUB:
+					begin
+						Rout= ry; ENR= 1; Gin= 1;
+						ALUcont= SUB;
 					end
-					default: begin
-						Rout= rx;
-						ENR= 1;
-						Ain= 0;
-						Gin= 1;
-						ALUcont = opINSTR; //assign val for FN
+					AND:
+					begin
+						Rout= ry; ENR= 1; Gin= 1;
+						ALUcont= AND;
+					end
+					OR:
+					begin
+						Rout= ry; ENR= 1; Gin= 1;
+						ALUcont= OR;
+					end
+					XOR:
+					begin
+						Rout= ry; ENR= 1; Gin= 1;
+						ALUcont= XOR;
+					end
+					LSL:
+					begin
+						Rout= ry; ENR= 1; Gin= 1;
+						ALUcont= LSL;
+					end
+					LSR:
+					begin
+						Rout= ry; ENR= 1; Gin= 1;
+						ALUcont= LSR;
+					end
+					ASR:
+					begin
+						Rout= ry; ENR= 1; Gin= 1;
+						ALUcont= ASR;
+					end
+					INV, FLP:
+					begin
+						Rin= rx; ENW= 1; Gout= 1; Clr= 1;
 					end
 				endcase
 				
 			end
 			//4TH TS
 			2'b11: begin
-				
-				//INV & FLP SHOULD NEVER GET 
-				//TO THE 4TH CLOCK PULSE
-				
-				case(INSTR) 
-					//ADD/SUB IMM
-					10'b10????????, 10'b11????????: begin
-						//IMM= INSTR[5:0];
-						Gin= 0;
-						ENR= 1;
-						Gout= 1;
-						ENW= 1;
-						Rin= rx;
-						Clr= 1;
-						
-					end
-					default: begin
-						Rin= rx;
-						ENR= 0;
-						Gin= 0;
-						Gout= 1;
-						ENW= 1;
-						Clr= 1;
-						ALUcont = opINSTR; //assign val for FN
-					end
-				endcase
-				
-			end
-			default: begin
-			//this is stupid
+				Gout= 1;
+				Rin= rx;
+				ENW= 1;
+				Clr= 1;
 			end
 			
 		endcase
-			
 	
 	
-
+	end
+	
 endmodule 
