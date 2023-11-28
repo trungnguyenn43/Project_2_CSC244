62c596f2a8000f67166cc6b2e9858f01bb25754d
Leo Ostigaard <105457849+losti3736@users.noreply.github.com>
2023-11-27 22:19:39 -0600

    Changed out_sig to s to keep things uniform :D

--- seven_seg.sv
+++ seven_seg.sv
@@ -17,7 +17,7 @@ seven_seg(.out_sig() , .a());
 
 module seven_seg(
 	input logic [3:0] a,
-	output logic [6:0] out_sig, output logic out_enable);
+	output logic [6:0] s, output logic out_enable);
 	
 	logic [15:0] y;
 	
@@ -42,13 +42,13 @@ module seven_seg(
 	assign	y[14] = (a[3] * a[2] * a[1] * ~a[0]) * in_enable;	//-2
 	assign	y[15] = (a[3] * a[2] * a[1] * a[0]) * in_enable;	//-1
 	
-	assign	out_sig[0] = ~(y[0] | y[2] | y[3] | y[5] | y[6] | y[7] | y[8] | y[9] | y[10] | y[12] | y[14] | y[15]);
-	assign	out_sig[1] = ~(y[0] | y[1] | y[2] | y[3] | y[4] | y[7] | y[8] | y[9] | y[10] | y[13]);
-	assign	out_sig[2] = ~(y[0] | y[1] | y[3] | y[4] | y[5] | y[6] | y[7] | y[8] | y[9] | y[10] | y[11] | y[13]);
-	assign	out_sig[3] = ~(y[0] | y[2] | y[3] | y[5] | y[6] | y[8] | y[9] | y[11] | y[12] | y[13] | y[14]);
-	assign	out_sig[4] = ~(y[0] | y[2] | y[6] | y[8] | y[10] | y[11] | y[12] | y[13] | y[14] | y[15]);
-	assign	out_sig[5] = ~(y[0] | y[4] | y[5] | y[6] | y[8] | y[9] | y[10] | y[11] | y[12] | y[14] | y[15]);
-	assign	out_sig[6] = ~(y[2] | y[3] | y[4] | y[5] | y[6] | y[8] | y[9] | y[10] | y[11] | y[13] | y[14] | y[15]);
+	assign	s[0] = ~(y[0] | y[2] | y[3] | y[5] | y[6] | y[7] | y[8] | y[9] | y[10] | y[12] | y[14] | y[15]);
+	assign	s[1] = ~(y[0] | y[1] | y[2] | y[3] | y[4] | y[7] | y[8] | y[9] | y[10] | y[13]);
+	assign	s[2] = ~(y[0] | y[1] | y[3] | y[4] | y[5] | y[6] | y[7] | y[8] | y[9] | y[10] | y[11] | y[13]);
+	assign	s[3] = ~(y[0] | y[2] | y[3] | y[5] | y[6] | y[8] | y[9] | y[11] | y[12] | y[13] | y[14]);
+	assign	s[4] = ~(y[0] | y[2] | y[6] | y[8] | y[10] | y[11] | y[12] | y[13] | y[14] | y[15]);
+	assign	s[5] = ~(y[0] | y[4] | y[5] | y[6] | y[8] | y[9] | y[10] | y[11] | y[12] | y[14] | y[15]);
+	assign	s[6] = ~(y[2] | y[3] | y[4] | y[5] | y[6] | y[8] | y[9] | y[10] | y[11] | y[13] | y[14] | y[15]);
 	
 	
 endmodule 
